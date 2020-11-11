b297b42f8e1262492e02f3a7c02e62d995935a6f
esco <salgadoe7861@gmail.com>
2020-11-10 21:25:14 -0800

    foundation form

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var queryURL = "https://api.openbrewerydb.org/breweries?by_postal=94550";
+// Stores openbrewerydb API in a variable
+var queryURL = "https://api.openbrewerydb.org/breweries?by_city=";
+// Onclick function stores input into variable
+var input = document.getElementsById("city").value;
+console.log(input);
+
+// Calls the openbrewerydb API then console logs the response
 $.ajax({
     url: queryURL,
     method: "GET"
